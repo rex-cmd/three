f1e044b862a3f3ff5ca8c2e469107d1ca15bf415
Hannah <rexdouble9@gmail.coml>
2021-06-03 21:28:53 +0300

    mailer files  created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token
-    before_save { email.downcase! }
+    attr_accessor :remember_token, :activation_token
+    before_save   :downcase_email
+    before_create :create_activation_digest
+    #before_save { email.downcase! }
     validates(:name, presence: true, length:{maximum:50}) 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates(:email, presence: true, length:{maximum:225}, format:{with: VALID_EMAIL_REGEX}, 
@@ -29,4 +31,15 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+  private
+  #downcased email edress
+  def downcase_email
+    self.email=email.downcase
+  end
+  #creates and allocates activation token and digest
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
 end
